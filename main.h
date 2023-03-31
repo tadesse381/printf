dd2dd98123c42c803bb99ad5b0743151c8f69ea0
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-03-31 11:38:03 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+int _printf(const char *format, ...);
+typedef struct select
+{
+    char *sp;
+    int (*t)(char *format, va_list);
+}drive;
+int (*driver(char *format));
+#endif
