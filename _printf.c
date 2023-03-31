eb2abbabc984824a395f3a5eea652b66f1830691
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-03-31 11:50:05 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_op_func - identify function
+ * @s: funct
+ *
+ * Return: int
+ *
+ **/
+int _printf(const char *format, ...)
+{
+    int written = 0, (*drive)(char *, va_list);
+    char sp[3];
+
+    va_list pa;
+
+    va_start(pa, format)
+
+        return (written);
+}
+
