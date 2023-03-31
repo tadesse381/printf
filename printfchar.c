5c2084d3fb8196c0a031e98eeb17bb2bfc6d781b
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-03-31 12:10:36 -0700

    printfchar.c

---
+++ printfchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * printc - print char
+ * @pa: Input data.
+ * Return: Nothing.
+ */
+ int printchar(char *format, va_list pa)
+ {
+    (void)formart;
+    _putchar(va_arg(pa, int));
+    return (1);
+ }
+
