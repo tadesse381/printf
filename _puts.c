97a9e940ca532be1d19bff94019292bafec44adc
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-03-31 12:03:38 -0700

    _puts.c

---
+++ _puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _puts - count the characters on a string
+  * @str: a pointer the integer we want to set to 402
+  *
+  * Return: int
+  */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+
