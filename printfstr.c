9c092e60019b09e89e14612a2451db42ce3a0c9a
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-03-31 12:12:28 -0700

    printfstr.c

---
+++ printfstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int printstr(char *format, va_list pa)
+{
+    char *str = va_arg(pa, char *);
+    int count;
+
+    (void)format;
+
+    if (str == NULL)
+    {
+        string = "(null)";
+    }
+    count = _puts(string);
+    return (count);
+}
+
