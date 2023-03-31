8aec06a3d9fd77dafe117cedc1b991f60bc102bf
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-03-31 12:36:38 -0700

    _Printf.c

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
+ *
+ * Return: int
+ */
+typedef struct structprint
+{
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
+
+int _buffer(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+#endif
+
